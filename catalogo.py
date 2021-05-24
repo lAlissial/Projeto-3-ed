e789680cd3e4834a0b1a8e1c370525c56387ac3e
lAlissial <72537882+lAlissial@users.noreply.github.com>
2021-05-24 09:29:18 -0300

    okgo

--- catalogo.py
+++ catalogo.py
@@ -11,7 +11,6 @@ class Catalogo:
       self._direita = direita
       self._balanco = balanco
       self._pai = pai
-      # return
 
     # get
     @property
@@ -97,7 +96,6 @@ class Catalogo:
     self._raiz = None
     self._profundidade = None
     self._caractmax = None
-    return
 
   # get
   @property
@@ -129,8 +127,191 @@ class Catalogo:
   def caractmax(self, novo):
       self._caractmax = novo
 
-  def __str__(self):
-    self.ajeitandoUmasParadas()
+  def inserir(self, id, nome, ano, album):
+    if (id == None) or (nome == None) or (ano == None) or (album == None):
+      return
+    if (not self.raiz):
+      self.raiz = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album)
+      return
+    else:
+      self.inserirRecursivo(self.raiz, id, nome, ano, album)
+      return
+
+  def inserirRecursivo(self, no, id, nome, ano, album):
+    if (id == no.id):
+      return
+
+    if id < no.id:
+      if no.esquerda:
+        self.inserirRecursivo(no.esquerda, id, nome, ano, album)
+      else:
+        no.esquerda = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album, pai=no)
+        self.attbalanco(no.esquerda)
+    else:
+      if no.direita:
+        self.inserirRecursivo(no.direita, id, nome, ano, album)
+      else:
+        no.direita = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album, pai=no)
+        self.attbalanco(no.direita)
+    return
+
+  def attbalanco(self, no):
+    if (no.balanco > 1 or no.balanco < -1):
+      self.rebalanco(no)
+      return
+    if no.pai:
+      if no.pai.esquerda is no:
+        no.pai.balanco += 1
+      elif no.pai.direita is no:
+        no.pai.balanco -= 1
+
+      if no.pai.balanco != 0:
+        self.attbalanco(no.pai)
+
+  def rotacaoEsquerda(self, no):
+    novonoraiz = no.direita
+    no.direita = novonoraiz.esquerda
+    if (novonoraiz.esquerda):
+      novonoraiz.esquerda.pai = no
+    novonoraiz.pai = no.pai
+    if no is self.raiz:
+      self.raiz = novonoraiz
+    else:
+      if no.pai.esquerda is no:
+        no.pai.esquerda = novonoraiz
+      else:
+        no.pai.direita = novonoraiz
+    novonoraiz.esquerda = no
+    no.pai = novonoraiz
+    no.balanco = no.balanco + 1 - min(novonoraiz.balanco, 0)
+    novonoraiz.balanco = novonoraiz.balanco + 1 + max(no.balanco, 0)
+
+  def rotacaoDireita(self, no):
+    novonoraiz = no.esquerda
+    no.esquerda = novonoraiz.direita
+    if (novonoraiz.direita):
+      novonoraiz.direita.pai = no
+    novonoraiz.pai = no.pai
+    if no is self.raiz:
+      self.raiz = novonoraiz
+    else:
+      if no.pai.direita is no:
+        no.pai.direita = novonoraiz
+      else:
+        no.pai.esquerda = novonoraiz
+    novonoraiz.direita = no
+    no.pai = novonoraiz
+    no.balanco = no.balanco - 1 - max(novonoraiz.balanco, 0)
+    novonoraiz.balanco = novonoraiz.balanco - 1 + min(no.balanco, 0)
+
+  def rebalanco(self, no):
+    if no.balanco < 0:
+      if no.direita.balanco > 0:
+        self.rotacaoDireita(no.direita)
+        self.rotacaoEsquerda(no)
+      else:
+        self.rotacaoEsquerda(no)
+    elif no.balanco > 0:
+      if no.esquerda.balanco < 0:
+        self.rotacaoEsquerda(no.esquerda)
+        self.rotacaoDireita(no)
+      else:
+        self.rotacaoDireita(no)
+
+  def buscarId(self, data, no):
+    if not no:
+      return None
+    elif no.id == data:
+      return no
+    elif data > no.id:
+      return self.buscarId(data, no.direita)
+    elif data < no.id:
+      return self.buscarId(data, no.esquerda)
+
+  def calcAltura(self, raizita):
+    profunds_esq = 0
+    profunds_dir = 0
+    if raizita.esquerda:
+      profunds_esq = self.calcAltura(raizita.esquerda)
+    if raizita.direita:
+      profunds_dir = self.calcAltura(raizita._direita)
+    return 1 + max(profunds_esq, profunds_dir)
+
+  def guardandoArvoreLista(self, lista, arvore):
+    if arvore != None:
+      self.guardandoArvoreLista(lista, arvore.esquerda)
+      lista.append(arvore)
+      self.guardandoArvoreLista(lista, arvore.direita)
+    return lista
+
+  def ordemAlfabetica(self, lista, arvore):
+    array = self.guardandoArvoreLista(lista, arvore)
+    ordenada = sorted(array, key=lambda registro: registro._nome)
+    outputzin = ''
+    for e in ordenada:
+      outputzin += f'{e.nome}\n'
+    return outputzin
+
+  def buscarAnos(self, lista1, arvore, ano, lista2):
+    array = self.guardandoArvoreLista(lista1, arvore)
+    ordenada = sorted(array, key=lambda registro: registro._ano)
+    ini = 0
+    fim = len(ordenada)-1
+    meio = (ini + fim) // 2
+
+    quebrando1 = ordenada[:meio]
+    ini1 = 0
+    fim1 = len(quebrando1)-1
+    while ini1 <= fim1:
+      meio1 = (ini1 + fim1) // 2
+      if ano >= quebrando1[meio1].ano:
+        if ano == quebrando1[meio1].ano:
+          lista2.append(quebrando1[meio1])
+        ini1 = meio1 + 1
+      else:
+        fim1 = meio1 - 1
+
+    quebrando2 = ordenada[meio:]
+    ini2 = 0
+    fim2 = len(quebrando2)-1
+    while ini2 <= fim2:
+      meio2 = (ini2 + fim2) // 2
+      if ano >= quebrando2[meio2].ano:
+        if ano == quebrando2[meio2].ano:
+          lista2.append(quebrando2[meio2])
+        ini2 = meio2 + 1
+      else:
+        fim2 = meio2 - 1
+
+    #busca resto
+    for i in ordenada:
+      if i.ano == ano:
+        if i not in lista2:
+          lista2.append(i)
+
+    return lista2
+
+  def calcProfeCarac(self):
+    if (not self.raiz):
+      self._profundidade = 0
+      self._caractmax = 1
+      return
+    self._profundidade = 0
+    self._caractmax = 1
+    l= []
+    l.append((self.raiz, 1, len(str(self.raiz.id))))
+    while len(l):
+      no, profundidade, chars = l.pop(0)
+      self._profundidade = max(self._profundidade, profundidade)
+      self._caractmax = max(self._caractmax, chars)
+      if no.esquerda:
+        l.append((no.esquerda, profundidade + 1, len(str(no.esquerda.id))))
+      if no.direita:
+        l.append((no.direita, profundidade + 1, len(str(no.direita.id))))
+    return
+  
+  def __str__(self): #percorre e printa
+    self.calcProfeCarac()
     if (self._profundidade == 0):
       return ""
     s = ""
@@ -140,7 +321,7 @@ class Catalogo:
     while len(poPrint):
       nolev, no = poPrint.pop(0)
       if (not no):
-        if ((self._profundidade - nolev + 1) <= 0):
+        if (self._profundidade - nolev + 1) <= 0:
           continue
         if (nolev != level):
           s += "\n"
@@ -169,175 +350,3 @@ class Catalogo:
         poPrint.append((nolev + 1, None))
     s += "\n"
     return s
-
-  def ajeitandoUmasParadas(self):
-    if (not self._raiz):
-      self._profundidade = 0
-      self._caractmax = 1
-      return
-    self._profundidade = 0
-    self._caractmax = 1
-    Q = []
-    Q.append((self._raiz, 1, len(str(self._raiz.id))))
-    while len(Q):
-      no, profundidade, chars = Q.pop(0)
-      self._profundidade = max(self._profundidade, profundidade)
-      self._caractmax = max(self._caractmax, chars)
-      if no.esquerda:
-        Q.append((no.esquerda, profundidade + 1, len(str(no.esquerda.id))))
-      if no.direita:
-        Q.append((no.direita, profundidade + 1, len(str(no.direita.id))))
-    return
-
-  # def contains(self, data):
-  #   return True if self.recursiveContains(data, self._raiz) else False
-
-  def inserir(self, id, nome, ano, album):
-    if (id == None) and (nome == None) and (ano == None) and (album == None):
-      return
-    if (not self._raiz):
-      self.raiz = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album)
-      return
-    else:
-      self._aju = 0
-      self.inserirRecursivo(self._raiz, id, nome, ano, album)
-      delattr(self, "_aju")
-      return
-
-  def inserirRecursivo(self, no, id, nome, ano, album):
-    if (id == no.id):
-      return
-
-    if id < no.id:
-      if no.esquerda:
-        self.inserirRecursivo(no.esquerda, id, nome, ano, album)
-      else:
-        no.esquerda = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album, pai = no)
-        self.attbalanco(no.esquerda)
-    else:
-      if no.direita:
-        self.inserirRecursivo(no.direita, id, nome, ano, album)
-      else:
-        no.direita = Catalogo.Registro(id=id, nome=nome, ano=ano, album=album, pai=no)
-        self.attbalanco(no.direita)
-    return
-
-  def attbalanco(self, no):
-    if (no.balanco > 1 or no.balanco < -1):
-      self.rebalanco(no)
-      return
-    if no.pai:
-      if no.pai.esquerda is no:
-        no.pai.balanco += 1
-      elif no.pai.direita is no:
-        no.pai.balanco -= 1
-
-      if no.pai.balanco != 0:
-        self.attbalanco(no.pai)
-
-  def rotacaoEsquerda(self, no):
-    print("rotating esquerda around: " + str(no.id))
-    novonoraiz = no.direita
-    no.direita = novonoraiz.esquerda
-    if (novonoraiz.esquerda):
-      novonoraiz.esquerda.pai = no
-    novonoraiz.pai = no.pai
-    if no is self._raiz:
-      self.raiz = novonoraiz
-    else:
-      if no.pai.esquerda is no:
-        no.pai.esquerda = novonoraiz
-      else:
-        no.pai.direita = novonoraiz
-    novonoraiz.esquerda = no
-    no.pai = novonoraiz
-    no.balanco = no.balanco + 1 - min(novonoraiz.balanco, 0)
-    novonoraiz.balanco = novonoraiz.balanco + 1 + max(no.balanco, 0)
-
-  def rotacaoDireita(self, no):
-    print("rotating direita around: " + str(no.id))
-    novonoraiz = no.esquerda
-    no.esquerda = novonoraiz.direita
-    if (novonoraiz.direita):
-      novonoraiz.direita.pai = no
-    novonoraiz.pai = no.pai
-    if no is self._raiz:
-      self.raiz = novonoraiz
-    else:
-      if no.pai.direita is no:
-        no.pai.direita = novonoraiz
-      else:
-        no.pai.esquerda = novonoraiz
-    novonoraiz.direita = no
-    no.pai = novonoraiz
-    no.balanco = no.balanco - 1 - max(novonoraiz.balanco, 0)
-    novonoraiz.balanco = novonoraiz.balanco - 1 + min(no.balanco, 0)
-
-  def rebalanco(self, no):
-    if no.balanco < 0:
-      if no.direita.balanco > 0:
-        self.rotacaoDireita(no.direita)
-        self.rotacaoEsquerda(no)
-      else:
-        self.rotacaoEsquerda(no)
-    elif no.balanco > 0:
-      if no.esquerda.balanco < 0:
-        self.rotacaoEsquerda(no.esquerda)
-        self.rotacaoDireita(no)
-      else:
-        self.rotacaoDireita(no)
-
-  def guardandoArvoreLista(self, lista, arvore):
-    if arvore != None:
-      self.guardandoArvoreLista(lista, arvore.esquerda)
-      lista.append(arvore)
-      self.guardandoArvoreLista(lista, arvore.direita)
-    return lista
-
-  def buscarId(self, data, no):
-    if not no:
-      return None
-    elif no.id == data:
-      return no
-    elif data > no.id:
-      return self.buscarId(data, no.direita)
-    elif data < no.id:
-      return self.buscarId(data, no.esquerda)
-
-  def ordemAlfabetica(self, lista, arvore):
-    array = self.guardandoArvoreLista(lista, arvore)
-    ordenada = sorted(array, key=lambda registro: registro._nome)
-    outputzin = ''
-    for e in ordenada:
-      outputzin += f'{e.nome}\n'
-    return outputzin
-
-  def buscarAnos(self, lista1, arvore, ano, lista2):
-    array = self.guardandoArvoreLista(lista1, arvore)
-    ordenada = sorted(array, key=lambda registro: registro._ano)
-    ini = 0
-    fim = len(ordenada)-1
-    while ini <= fim:
-      meio = (ini + fim) // 2
-      if ano >= ordenada[meio]._ano:
-        if ano == ordenada[meio]._ano:
-          lista2.append(ordenada[meio])
-        ini = meio + 1
-      else:
-        fim = meio - 1
-    return lista2
-
-  # def buscarPorAno(self, ano, no):
-  #   registros = []
-  #   inicio = self._raizAno
-  #   while inicio:
-  #     if ano >= inicio._ano:
-  #       if ano == inicio._ano:
-  #         registros.append(inicio)
-  #       inicio = inicio._direita
-  #     else:
-  #       inicio = inicio._esquerda
-  #   return registros
-
-
-
